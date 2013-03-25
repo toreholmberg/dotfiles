41f5779dacea80ccaf9983748227ad225e3e9db9
Tore Holmberg <tore.holmberg@gmail.com>
2013-03-25 17:31:47 -0400

    added some colors

--- shell/profile.sh
+++ shell/profile.sh
@@ -1,2 +1,9 @@
 # enable rvm
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
+
+# Tell ls to be colourful
+export CLICOLOR=1
+export LSCOLORS=cxfxcxdxbxegedabagacad
+
+# Tell grep to highlight matches
+export GREP_OPTIONS='--color=auto'
