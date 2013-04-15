53071c98ee66e849d79f8cc40c4d937acd77753b
Tore Holmberg <tore.holmberg@gmail.com>
2013-04-15 18:12:32 -0400

    recursively search files for string command

--- shell/profile.sh
+++ shell/profile.sh
@@ -5,3 +5,8 @@
 export CLICOLOR=1
 export LSCOLORS=GxFxCxDxBxegedabagaced
 export GREP_OPTIONS='--color=auto'
+
+# recursively search all files in director for a stringfind . -type f -print0 | xargs -0 grep -l "channel-none"
+function search() {
+	find . -type f -print0 | xargs -0 grep -l "$@"
+}
