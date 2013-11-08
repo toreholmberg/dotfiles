7f425e94fa5966fb776e9722e508919ae57be1e3
Tore Holmberg <tore.holmberg@gmail.com>
2013-11-08 14:31:15 -0500

    added python -m SimpleHTTPServer as alias

--- aliases/osxdev.sh
+++ aliases/osxdev.sh
@@ -4,3 +4,5 @@ alias flushdns='dscacheutil -flushcache'
 # open chrome with web security disabled
 alias chrome_no_security="open -a '/Applications/Google Chrome.app' --args --disable-web-security"
 
+# simple http server
+alias simpleserver='python -m SimpleHTTPServer'
