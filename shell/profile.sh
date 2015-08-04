ab86154745912c649d9efd6ab45960b338a521cc
Tore Holmberg <tore.holmberg@gmail.com>
2015-08-04 12:24:56 -0400

    adds remove_ds_store

--- shell/profile.sh
+++ shell/profile.sh
@@ -26,5 +26,10 @@ export GREP_OPTIONS='--color=auto'
 
 # recursively search all files in director for a string
 function search() {
-	find . -type f -print0 | xargs -0 grep -l "$@"
+  find . -type f -print0 | xargs -0 grep -l "$@"
+}
+
+# remove .DS_Store files
+function remove_ds_store() {
+  find . -name '*.DS_Store' -type f -delete
 }
