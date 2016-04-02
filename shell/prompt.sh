4c52164ee9710eedcad889369c2d2db240b5acf3
Tore Holmberg <tore.holmberg@gmail.com>
2016-04-02 19:01:34 -0400

    updates prompt

---
+++ shell/prompt.sh
@@ -0,0 +1,19 @@
+# Based on https://gist.github.com/srguiwiz/de87bf6355717f0eede5
+
+function git_branch {
+  git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'
+}
+
+function markup_git_branch {
+  if [[ "x$1" = "x" ]]; then
+    echo -e "$1"
+  else
+    if [[ $(git status 2> /dev/null | tail -n1) = "nothing to commit, working directory clean" ]]; then
+      echo -e ' \033[1;32m'"$1"'\033[0;30m'
+    else
+      echo -e ' \033[1;33m'"$1"'*\033[0;30m'
+    fi
+  fi
+}
+
+export PS1='\h$(markup_git_branch $(git_branch))\[\033[0m\] \W$ '
